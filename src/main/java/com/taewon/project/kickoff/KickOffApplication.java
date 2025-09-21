68671a4f9cc6a5720537a6a75f8371479e503f75
taewon.hong <40269387+taewonh@users.noreply.github.com>
2025-09-21 15:56:44 +0900

    init project

---
+++ src/main/java/com/taewon/project/kickoff/KickOffApplication.java
@@ -0,0 +1,13 @@
+package com.taewon.project.kickoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickOffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickOffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taewon/project/kickoff/KickOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taewon.project.kickoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickOffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
